83ae78104c6aa549396ae76cccc1cb51713a8184
Azeem10101 <arshadazeem2024@gmail.com>
2026-05-17 12:09:01 +0530

    Separate home screen from main app entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'features/meetup/presentation/screens/home_screen.dart';
 
 void main() {
   runApp(const MeetPointApp());
@@ -16,28 +17,6 @@ class MeetPointApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MeetPoint'),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to MeetPoint',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
     );
   }
 }
